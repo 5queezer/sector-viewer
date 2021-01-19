84b89b524120510ce5ce4e783d37a5af2d0999d7
Christian Pojoni <dev@pojoni.at>
2021-01-19 12:40:58 -0600

    icons

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store'
 
+import { icons } from './assets/icons'
+
+React.icons = icons
+
 ReactDOM.render(
   <Provider store={store}>
     <App/>
