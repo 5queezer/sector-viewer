6bc184eca388bd0dcbe5b539555b62fedcf486c9
Christian Pojoni <dev@pojoni.at>
2021-01-19 15:55:29 -0600

    test env vars for firebase

--- src/views/overview/Overview.js
+++ src/views/overview/Overview.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Overview = () => {
     return (
       <>
-      <span>Overview</span>
+      <span>API_HOST: {process.env.API_HOST}</span><br/>
+      <span>NODE_ENV: {process.env.NODE_ENV}</span><br/>
+      <span>GCLOUD_PROJECT: {process.env.GCLOUD_PROJECT}</span><br/>
+      <span>FIREBASE_CONFIG: {process.env.FIREBASE_CONFIG}</span><br/>
       </>
     )
 }
